fb1567dd27f267a9362996c66854d02d9af92d08
BabaxRhb <baba.randrianarisaona@gmail.com>
2026-05-29 15:22:49 +0300

    fix : review width and height for Carousel

--- src/shared/components/container/Carousel.tsx
+++ src/shared/components/container/Carousel.tsx
@@ -5,28 +5,22 @@ const Carousel = ({ imgUrl }: { imgUrl: string[] }) => {
 	const [ currentIndex, setCurrentIndex ] = useState(0);
 
 	const handlePrevious = () => {
-		console.log("previous", currentIndex);
 		setCurrentIndex((prevIndex) => (prevIndex === 0 ? imgUrl.length - 1 : prevIndex - 1));
 	}
 
 	const handleNext = () => {
-		console.log("next", currentIndex);
 		setCurrentIndex((prevIndex) => (prevIndex === imgUrl.length - 1 ? 0 : prevIndex + 1));
 	}
 
 	return (
-		<div className="relative w-50 h-50">
-			<div
-				className={`flex transition ease-out duration-40`}
-			>
-				<img src={imgUrl[currentIndex]} />
-			</div>
-			<div className="absolute top-0 h-full w-full justify-between items-center flex text-white phone:px-5 text-lg">
+		<div className="w-80 relative">
+			<img src={imgUrl[currentIndex]} className="w-full h-80 object-contain bg-transparent"/>
+			<div className="absolute top-0 h-full w-full justify-between items-center flex text-white">
 				{ currentIndex > 0 ? (
-					<button onClick={handlePrevious} className="bg-primary/30 rounded-full p-2">{"<"}</button>
+					<button onClick={handlePrevious} className="bg-primary/50 rounded-full p-0.5">{"<"}</button>
 				) : <div></div> }
 				{ currentIndex < imgUrl.length - 1 ? (
-					<button onClick={handleNext} className="bg-primary/30 rounded-full p-2">{">"}</button>
+					<button onClick={handleNext} className="bg-primary/50 rounded-full p-0.5">{">"}</button>
 				) : <div></div> }
 			</div>
 		</div>
